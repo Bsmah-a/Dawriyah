10a39cc38bce5b912d66cce9fd634e6b22593526
Bsmah-a <my.drdbes@gmail.com>
2023-11-27 14:07:43 +0300

    splashView

---
+++ Dawriyah/other/SplachScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplachScreenView.swift
+//  Dawriyah
+//
+//  Created by Bsmah Ali on 12/05/1445 AH.
+//
+
+import SwiftUI
+
+struct SplachScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplachScreenView()
+}

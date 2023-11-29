1ae7112e3f43ffc8fa9b5687244852bc73e911ab
Shahad <shabduz10@gmail.com>
2023-11-29 14:29:49 +0300

    Accept Regect Page

---
+++ Dawriyah/AcceptReject.swift
@@ -0,0 +1,27 @@
+//
+//  AcceptReject.swift
+//  Dawriyah
+//
+//  Created by Shahad Alzowaid on 15/05/1445 AH.
+//
+
+import SwiftUI
+
+struct AcceptReject: View {
+    var body: some View {
+        NavigationStack{
+            ZStack{
+                Color("backg").ignoresSafeArea()
+               
+                   
+                Image("profile").resizable().scaledToFill().frame(width: 10, height: 60).padding(.bottom,720).padding(.leading, 275)
+                   
+
+                
+            }.navigationTitle("Dwriyah Groups").foregroundColor(Color("Color2")).bold()
+        }
+    }}
+
+#Preview {
+    AcceptReject()
+}

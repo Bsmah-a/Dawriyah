73328c34d9ae9eeef4c5978f79fd4cf457e9b7fc
reemaalsmari <alsmarireema@gmail.com>
2023-11-29 17:00:47 +0300

    this is the profile pic button ( will be displayed in the groups page بالزاوية )

---
+++ Dawriyah/Account.swift
@@ -0,0 +1,18 @@
+//
+//  Account.swift
+//  Dawriyah
+//
+//  Created by Reema Alsmari on 29/11/2023.
+//
+
+import SwiftUI
+
+struct Account: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Account()
+}

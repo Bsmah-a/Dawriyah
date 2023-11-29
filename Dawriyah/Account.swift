ad88f25b7ba89e585f6210786ed3d377ca5d84d3
reemaalsmari <alsmarireema@gmail.com>
2023-11-29 17:15:44 +0300

    this is the profile button on the groups page (corner)

--- Dawriyah/Account.swift
+++ Dawriyah/Account.swift
@@ -8,8 +8,92 @@
 import SwiftUI
 
 struct Account: View {
+    
+    @State private var isShowingProfileSheet = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Profile Picture Button
+            Button(action:   {
+                isShowingProfileSheet.toggle()
+                
+            }) {
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                    .foregroundColor(.brown)
+            }
+            .sheet(isPresented: $isShowingProfileSheet) {
+                ProfileSheet()
+            }
+        }
+        .padding()
+    }
+}
+
+struct ProfileSheet: View {
+    @State private var isShowingAccountSettings = false
+    @State private var isShowingInvitations = false
+
+    var body: some View {
+        NavigationView {
+            List {
+                // Account Settings Button
+                Button(action: {
+                    isShowingAccountSettings.toggle()
+                    
+                }) {
+                    Text("Account Settings")
+                    .foregroundColor(.brown)
+                }
+                .sheet(isPresented: $isShowingAccountSettings) {
+                    AccountSettingsView()
+                }
+
+                // Invitations Button
+                Button(action: {
+                    isShowingInvitations.toggle()
+                }) {
+                    Text("Invitations")
+                    .foregroundColor(.brown)
+                    
+                }
+                .sheet(isPresented: $isShowingInvitations) {
+                    InvitationsView()
+                }
+            }
+            .navigationTitle("Profile")
+//            .navigationBarItems(trailing: Button("Done") {
+//                // Action when "Done" button is tapped
+//                // You can perform any necessary actions here
+//                // For example, dismissing the sheet
+//                dismiss()
+//            })
+        }
+    }
+
+//    private func dismiss() {
+//        // Function to dismiss the sheet
+//        // You can customize it based on your needs
+//        // Here, we simply set the isShowingProfileSheet to false
+//        // to dismiss the sheet
+//       // isShowingProfileSheet = false
+//    }
+}
+
+struct AccountSettingsView: View {
+    var body: some View {
+        Text("Account Settings")
+            .padding()
+    }
+}
+
+struct InvitationsView: View {
+    var body: some View {
+        Text("Invitations")
+            .padding()
     }
 }
 

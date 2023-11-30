3b774e14292e8d61ed4ea4a13fe3fb262ad7d9ab
RenadAlqarni <renadsalqarni@gmail.com>
2023-11-30 11:57:35 +0300

    BusyMembers(), DawriahTurns()

--- Dawriyah/BusyMembers.swift
+++ Dawriyah/BusyMembers.swift
@@ -9,10 +9,36 @@ import SwiftUI
 
 struct BusyMembers: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            
+            VStack{
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .frame(width:350,height: 1)
+                    .padding(.vertical,-300)
+                
+                Button {
+                    Text("add user")
+                                    } label: {
+                                        Image(systemName: "plus.app").foregroundColor(Color("Color2"))
+                                            .bold()
+                                    }.offset(x: 150, y: -330)
+                
+                Text("Looks like this day is free !")
+                    .foregroundColor(.gray)
+                    .font(.title)
+                
+                
+                
+                
+            }.navigationTitle("Busy Members")
+            
+            
+            
+        }
+        
     }
 }
-
 #Preview {
     BusyMembers()
 }
--- Dawriyah/DawriyahTurns.swift
+++ Dawriyah/DawriyahTurns.swift
@@ -4,12 +4,35 @@
 //
 //  Created by Renad Alqarni on 27/11/2023.
 //
-
 import SwiftUI
 
 struct DawriyahTurns: View {
+    @State private var items = ["Raseel", "Jude", "Haifa", "Renad"]
+    @State private var memoji = ["memoji1", "memoji2", "memoji3", "memoji1"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Drag and drop to arrange order")
+                    .font(.title2)
+                    .padding(.vertical, 10)
+                    .offset(x: -33)
+                
+                List {
+                    Section(header: Text("edit order")) {
+                        ForEach(items, id: \.self) { item in
+                            Text(item.capitalized)
+                        }
+                        .onMove { indices, newOffset in
+                            items.move(fromOffsets: indices, toOffset: newOffset)
+                        }
+                    }
+                }
+                .navigationTitle("Dawriyah Turns")
+                .navigationBarItems(leading: EditButton())
+            }
+            .accentColor(Color("Color2"))
+        }
     }
 }
 

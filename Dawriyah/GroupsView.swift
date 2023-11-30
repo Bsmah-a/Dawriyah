ec408cfc4f984049c5a6d7cf8cfcb28acac0a3b7
RenadAlqarni <renadsalqarni@gmail.com>
2023-11-30 15:16:52 +0300

    groups, dawriyahdaysheet

--- Dawriyah/GroupsView.swift
+++ Dawriyah/GroupsView.swift
@@ -8,11 +8,67 @@
 import SwiftUI
 
 struct GroupsView: View {
+    
+    @State private var isShowingProfileSheet = false
+    
+    let groupName:[String]=["family", "Friends1", "Friend2"]
+    let peoples:[ peopleInfo] = [
+        peopleInfo(emoji: 1, name: "Renad"),
+        peopleInfo(emoji: 2, name: "Basemah"),
+        peopleInfo(emoji: 3, name: "Reema"),
+        peopleInfo(emoji: 1, name: "Taif"),
+        peopleInfo(emoji: 1, name: "Sara")
+    ]
+   
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                
+                Button(action:{
+                            isShowingProfileSheet.toggle()
+                        }) {
+                            Image(systemName: "person.circle").font(.largeTitle).foregroundColor(Color("Color2")).padding(.top, -76.0).padding(.leading, 290.0)}
+                        
+                        .sheet(isPresented: $isShowingProfileSheet) {
+                            ProfileSheet()
+                        }
+                   
+            ScrollView{
+                
+                    HStack{Spacer()}
+                    ForEach(groupName, id: \.self) { group in
+                        ZStack{
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).frame(width: 312, height: 140).foregroundColor(Color("Color2")).opacity(0.40)
+                            VStack(alignment: .leading, spacing: 20){
+                                Text(group).padding(.leading, 15.0).foregroundColor(Color("TitleC")).bold().font(.title2)
+                                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).frame(width: 300, height: 2).foregroundColor(.gray).opacity(0.30)
+                                
+                                HStack(spacing:-20){
+                                    ForEach(peoples) { person in
+                                        Image("memoji\(person.emoji)")}}.padding(.leading, 20.0)
+
+                            }
+                            }
+                        }
+
+                   
+                    ZStack{
+                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).frame(width: 312, height: 125).foregroundColor(Color("Color2")).opacity(0.40)
+                        NavigationLink(destination: CreateView()){
+                            Image(systemName: "plus.circle").font(.largeTitle).foregroundColor(Color("Color2")).opacity(0.40)}
+                    }
+                    
+                    Spacer()
+            }.padding(.top, 40.0)
+                }.navigationTitle("Dawriyah Groups")
+                .background{
+                    Color("backg").ignoresSafeArea()
+                }
+                
+        }.accentColor(Color("Color2"))
+
     }
 }
-
 #Preview {
     GroupsView()
 }

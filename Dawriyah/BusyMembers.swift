138c38469ba47442cb043a7e460d4f7c06f9fafe
RenadAlqarni <renadsalqarni@gmail.com>
2023-11-30 11:55:17 +0300

    BusyMembers

---
+++ Dawriyah/BusyMembers.swift
@@ -0,0 +1,18 @@
+//
+//  BusyMembers.swift
+//  Dawriyah
+//
+//  Created by Renad Alqarni on 30/11/2023.
+//
+
+import SwiftUI
+
+struct BusyMembers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BusyMembers()
+}

cc408d808c21e93be4f11e58e45d7929b65b2481
RenadAlqarni <renadsalqarni@gmail.com>
2023-11-30 15:01:01 +0300

    GroupsView

---
+++ Dawriyah/DawriyahDaySheet.swift
@@ -0,0 +1,18 @@
+//
+//  DawriyahDaySheet.swift
+//  Dawriyah
+//
+//  Created by Renad Alqarni on 30/11/2023.
+//
+
+import SwiftUI
+
+struct DawriyahDaySheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DawriyahDaySheet()
+}

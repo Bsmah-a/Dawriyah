e79764bc29977f1ce25e9826abb6e70614e578e3
Shahad <shabduz10@gmail.com>
2023-12-04 23:08:36 +0300

    back button been added here in the top left corner .. no link yet but I will navigate it in the morning inshallah No worrys :)

--- Dawriyah/Views/CalanderView.swift
+++ Dawriyah/Views/CalanderView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct CalendarView: View {
+    @Environment(\.presentationMode) var presentationMode
     let calendar = Calendar.current
     let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     @State private var selectedDate: Date = Date()
@@ -22,44 +23,36 @@ struct CalendarView: View {
         peopleInfo(emoji: 1, name: "Taif")]
     @State private var busyMembers: [peopleInfo] = []
     @State private var busyDays: Set<Date> = []
-    
-    
-    
+
     var body: some View {
         NavigationView {
-            // groups
             VStack {
-               
                 Rectangle()
                     .foregroundColor(.gray)
-                    .frame(width:350,height: 1)
-                    .padding(.vertical,-60)
-                
+                    .frame(width: 350, height: 1)
+                    .padding(.vertical, -60)
+
                 Text("Dawriyah turns")
                     .font(.title2)
-                    .padding(.vertical,-60)
-                    .offset(x:-100)
-                
-                ZStack{
-                    
-                    
+                    .padding(.vertical, -60)
+                    .offset(x: -100)
+
+                ZStack {
                     Rectangle()
                         .foregroundColor(Color("Color3"))
                         .frame(width: 340, height: 60)
                         .cornerRadius(15)
                         .offset(x: 5, y: -40)
                         .padding()
-                    
-                    
-                    
-                    HStack(spacing: 10){
+
+                    HStack(spacing: 10) {
                         ForEach(people) { person in
                             Image("memoji\(person.emoji)")
-                                .offset(x:-10,y:-40)
+                               
+                                .offset(x: -10, y: -40)
                         }
-                        
                     }
-                    
+
                     Button {
                         showSheet = true
                     } label: {
@@ -70,57 +63,69 @@ struct CalendarView: View {
                         DawriyahTurns()
                     })
                 }
-                
-                
+
                 Rectangle()
                     .foregroundColor(.gray)
-                    .frame(width:350,height: 1)
-                    .padding(.vertical,-45)
-                
+                    .frame(width: 350, height: 1)
+                    .padding(.vertical, -45)
+
                 Text("Highlight your busy days !")
                     .font(.title2)
-                    .padding(.vertical,-50)
-                    .offset(x:-50)
-                
+                    .padding(.vertical, -50)
+                    .offset(x: -50)
                 //calendar
                 VStack() {
                     Divider().frame(height: 1)
                     DatePicker("Select Date", selection: $selectedDate, displayedComponents: [.date])
                         .padding(.horizontal)
                         .datePickerStyle(.graphical)
-                        .onChange(of: selectedDate) { newValue in
+                        .accentColor(Color.brown)
+                        .onChange(of: selectedDate) { _ in
                             checkAvailability()
-                            }
-                    
+                        }
                     Divider()
                 }
                 .sheet(isPresented: $isSheetPresented) {
                     BusyMembers(busyMembers: $busyMembers)
                 }
-            }.navigationTitle("Family").toolbar {
-                Button(action:{
-                    DawriyahSheet.toggle()
-                }) {
-                    Image(systemName: "pin.circle")
-                        .font(.largeTitle)
-                        .foregroundColor(Color("Color2"))
-                }}
-            .sheet(isPresented: $DawriyahSheet, content: {
-                DawriyahDaySheet()})
-            
-        }.accentColor(Color("Color2"))
-        
+            }
+            .navigationTitle("Family")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image(systemName: "chevron.left").foregroundColor(Color("Color2"))
+                        Text("Back").foregroundColor(Color("Color2"))
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        DawriyahSheet.toggle()
+                    }) {
+                        Image(systemName: "pin.circle")
+                            .font(.largeTitle)
+                            .foregroundColor(Color("Color2"))
+                    }
+                }
+            }
+            .sheet(isPresented: $DawriyahSheet) {
+                DawriyahDaySheet()
+            }
+        }
     }
-  
+
     func checkAvailability() {
         let isDateAvailable = true
-        
+
         if isDateAvailable {
             isSheetPresented = true
         }
     }
-
 }
-#Preview {
-    CalendarView()
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
 }
